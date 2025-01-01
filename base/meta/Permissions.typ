3a378a853ac7b59370f3772cf51f1585901a8665
Kyle Ladd <kyle@ladd.io>
2025-01-01 09:37:26 -0500

    Start using Typst for pages

---
+++ base/meta/Permissions.typ
@@ -0,0 +1,15 @@
+Permissions dictate how #link("/meta/users", "users") can view and edit pages or
+#link("/meta/namespaces", "namespaces").
+
+== Page permissions
+
+TODO: Document
+
+== Namespace permissions
+
+TODO: Document
+
+== Admin roles
+
+The "meta" user is the root user of the wiki, and has privileges for all
+namespaces and users.
--- src/view.rs
+++ src/view.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::sync::{Arc, LazyLock};
 
 use askama::Template;
 use axum::{
@@ -6,6 +6,15 @@ use axum::{
 	response::{Html, IntoResponse},
 };
 use axum_extra::{headers, TypedHeader};
+use typst::{
+	diag::{FileError, FileResult, Warned},
+	foundations::{Bytes, Datetime},
+	html::{HtmlDocument, HtmlElement, HtmlNode},
+	syntax::{FileId, Source, VirtualPath},
+	text::{Font, FontBook},
+	utils::LazyHash,
+	Feature, Features, Library, World,
+};
 
 use crate::{
 	auth,
@@ -19,12 +28,74 @@ use crate::{
 };
 
 #[derive(Template)]
-#[template(path = "view.html")]
+#[template(path = "view.html", escape = "none")]
 pub struct ViewTemplate<'a> {
 	pub(crate) title: &'a str,
 	pub(crate) body: &'a str,
 	pub(crate) slug: &'a str,
 	pub user: Option<UserView>,
+}
+
+static LIBRARY: LazyLock<LazyHash<Library>> = LazyLock::new(|| {
+	LazyHash::new(
+		Library::builder()
+			.with_features(vec![Feature::Html].into_iter().collect())
+			.build(),
+	)
+});
+
+static FONTS: LazyLock<LazyHash<FontBook>> =
+	LazyLock::new(|| LazyHash::new(FontBook::new()));
+
+struct SandboxWorld {
+	file_id: FileId,
+	source: Source,
+}
+
+impl SandboxWorld {
+	pub fn new(text: &str) -> Self {
+		let file_id = FileId::new(None, VirtualPath::new("main.typ"));
+		let source = Source::new(file_id, text.to_string());
+		Self { file_id, source }
+	}
+}
+
+impl World for SandboxWorld {
+	fn library(&self) -> &LazyHash<Library> {
+		&LIBRARY
+	}
+
+	fn book(&self) -> &LazyHash<FontBook> {
+		&FONTS
+	}
+
+	fn main(&self) -> FileId {
+		self.file_id
+	}
+
+	fn source(&self, id: FileId) -> FileResult<Source> {
+		if id == self.file_id {
+			Ok(self.source.clone())
+		} else {
+			Err(FileError::NotFound(id.vpath().as_rootless_path().into()))
+		}
+	}
+
+	fn file(&self, id: FileId) -> FileResult<Bytes> {
+		if id == self.file_id {
+			Ok(self.source.text().as_bytes().into())
+		} else {
+			Err(FileError::NotFound(id.vpath().as_rootless_path().into()))
+		}
+	}
+
+	fn font(&self, _index: usize) -> Option<Font> {
+		None
+	}
+
+	fn today(&self, _offset: Option<i64>) -> Option<Datetime> {
+		None
+	}
 }
 
 pub async fn get(
@@ -47,10 +118,17 @@ pub async fn get(
 			return not_found().await.into_response();
 		}
 
+		let world = SandboxWorld::new(page.content());
+		let Warned { output, warnings } =
+			typst::compile::<HtmlDocument>(&world);
+		// TODO: unwrap
+		let result = output.and_then(|doc| typst_html::html(&doc)).unwrap();
+		dbg!(warnings);
+
 		Html(
 			ViewTemplate {
 				title: page.title(),
-				body: page.content(),
+				body: &result,
 				slug: page.slug(),
 				user: user.map(UserView::new),
 			}
